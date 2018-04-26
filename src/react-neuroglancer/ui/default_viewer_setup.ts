7bcd68c74a8057d9f7a68495f7cb835b6a7d066a
armin.ytaheri@gmail.com <armin.ytaheri@gmail.com>
2018-04-26 16:59:59 -0400

    Default viewer takes HTMLElement as an argument

---
+++ src/react-neuroglancer/ui/default_viewer_setup.ts
@@ -0,0 +1,58 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { StatusMessage } from "neuroglancer/status";
+import {
+  bindDefaultCopyHandler,
+  bindDefaultPasteHandler
+} from "neuroglancer/ui/default_clipboard_handling";
+import { setDefaultInputEventBindings } from "neuroglancer/ui/default_input_event_bindings";
+import { makeDefaultViewer } from "react-neuroglancer/ui/default_viewer";
+import { UrlHashBinding } from "neuroglancer/ui/url_hash_binding";
+import { ViewerOptions } from "neuroglancer/viewer";
+
+/**
+ * Sets up the default neuroglancer viewer.
+ */
+export function setupDefaultViewer(
+  root: HTMLElement | null,
+  options?: Partial<ViewerOptions>
+) {
+  if (!root) {
+    throw new Error("The given root HTML element is undefined.");
+  }
+  let viewer = ((<any>window)["viewer"] = makeDefaultViewer(root, options));
+  setDefaultInputEventBindings(viewer.inputEventBindings);
+
+  const hashBinding = viewer.registerDisposer(new UrlHashBinding(viewer.state));
+  viewer.registerDisposer(
+    hashBinding.parseError.changed.add(() => {
+      const { value } = hashBinding.parseError;
+      if (value !== undefined) {
+        const status = new StatusMessage();
+        status.setErrorMessage(`Error parsing state: ${value.message}`);
+        console.log("Error parsing state", value);
+      }
+      hashBinding.parseError;
+    })
+  );
+  hashBinding.updateFromUrlHash();
+
+  bindDefaultCopyHandler(viewer);
+  bindDefaultPasteHandler(viewer);
+
+  return viewer;
+}
